ff6acf608e86869a3f2c0997b9b7e2ce319e5500
Mike Harley <mharley12345@gmail.com>
2019-08-06 12:20:28 -0700

    adds GS cdn andmoveLogo animation

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-// Your code goes here
+let moveLogo = document.querySelector('.logo-heading');
+
+
+
+
+// Move Logo
+
+TweenMax.to(moveLogo, 5,{x:1220,y:4,skewX:45,rotation:360});
+// move logo back
+
+setTimeout(()=>{
+    TweenMax.to(moveLogo,5,{x:46,y:4,scale:2,skewX:45,rotation:360});
+
+}, 15000);
